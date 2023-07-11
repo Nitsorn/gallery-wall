e684d91d925cf227409e3b01bf2327f26e8b4b7e
Nitsorn <nitsorn.oat@gmail.com>
2023-07-11 17:30:28 -0400

    clean up applyication

--- content.tsx
+++ content.tsx
@@ -9,10 +9,14 @@ export const getStyle = () => {
 }
 
 function Settings() {
-  const [wallColor, setWallColor] = useState("#a0aec0")
-  const [frameColor, setFrameColor] = useState("#000000")
-  const [countPerRow, setCountPerRow] = useState(6)
-  const [frameWidth, setFrameWidth] = useState(5)
+
+  const [wallColor, setWallColor] = useState(localStorage.getItem('wallColor') || "#e3e3e3")
+  const [frameColor, setFrameColor] = useState(localStorage.getItem('frameColor') || "#000000")
+  const [countPerRow, setCountPerRow] = useState(localStorage.getItem('countPerRow') || 4)
+  const [frameWidth, setFrameWidth] = useState(Number(localStorage.getItem('frameWidth')) || 5)
+
+  console.log(wallColor, frameColor, countPerRow, frameWidth);
+  
 
   const draw = useCallback(() => {
     if (document.querySelector('main') === null) return;
@@ -33,24 +37,38 @@ function Settings() {
        pathname.includes('new-in') ||
        pathname.includes('special-offer') ||
       pathname.includes('wishlist');
+
+    const applyWallColor = (element) => {
+      element.setAttribute('style', `background: var(--gw-wall-color);`)
+    }
+
+    const applyFrameColorAndShadow = (element) => {
+      element.setAttribute('style', `box-shadow: 4px 4px 5px 0px rgba(0,0,0,0.5) !important; ${element.getAttribute('style')}`);
+      if (supportFrameColor) {
+        element.setAttribute('style', `background-color: var(--gw-frame-color) !important; padding: var(--gw-frame-width) !important; ${element.getAttribute('style')}`);
+        if (element.children[0]) {
+          element.children[0].setAttribute('style', `box-shadow: inset 4px 4px 5px rgba(0,0,0,0.5) !important; ${element.children[0].getAttribute('style')}`);
+        }
+      }
+    }
     
     if (supportWallColor) {
-      document.querySelector('header[role="banner"]').setAttribute('style', `background: var(--gw-wall-color);`)
-      document.querySelector('main').setAttribute('style', `background: var(--gw-wall-color);`)
+      applyWallColor(document.querySelector('header[role="banner"]'))
+      applyWallColor(document.querySelector('main'))
 
       // for product detail view
       if (document.querySelector('main').querySelector('div.bg-brand-300') !== null) {
-        document.querySelector('main').querySelector('div.bg-brand-300').setAttribute('style', `background: var(--gw-wall-color);`)
+        applyWallColor(document.querySelector('main').querySelector('div.bg-brand-300'))
         
 
         document.querySelector('main').querySelector('div.bg-brand-300').querySelectorAll('a > span > img').forEach(img => {
-          img.setAttribute('style', `${ supportFrameColor ? 'border: var(--gw-frame-width) solid var(--gw-frame-color) !important;' : ''} box-shadow: 4px 4px 5px 0px rgba(0,0,0,0.5) !important; ${img.getAttribute('style')}`);
+          applyFrameColorAndShadow(img);
         });
       }
 
       // for recently viewed carousel
       document.querySelectorAll('ul > li article div.absolute').forEach(article => {
-        article.setAttribute('style', `${ supportFrameColor ? 'border: var(--gw-frame-width) solid var(--gw-frame-color); !important' : ''} box-shadow: 4px 4px 5px 0px rgba(0,0,0,0.5) !important; ${article.getAttribute('style')}`);
+        applyFrameColorAndShadow(article);
       });
 
       const gridContainer = document.querySelector('.grid.w-full.gap-4');
@@ -58,21 +76,21 @@ function Settings() {
 
       document.querySelectorAll('section').forEach((section) => {
         
-        section.parentElement.setAttribute('style', `background: var(--gw-wall-color);`)
+        applyWallColor(section.parentElement)
 
         // for filter/sort sticky
         if (section.parentElement.querySelector('div.sticky') !== null) {
-          section.parentElement.querySelector('div.sticky').setAttribute('style', `background: var(--gw-wall-color);`)
+          applyWallColor(section.parentElement.querySelector('div.sticky'))
         }
         const articles = section.querySelectorAll('article div.absolute');
 
         articles.forEach((article) => {
-          article.setAttribute('style', `${ supportFrameColor ? 'border: var(--gw-frame-width) solid var(--gw-frame-color); !important' : ''} box-shadow: 4px 4px 5px 0px rgba(0,0,0,0.5) !important; ${article.getAttribute('style')}`);
+          applyFrameColorAndShadow(article);
         })
       });
     }
 
-   
+
 
   }, []);
 
@@ -82,10 +100,10 @@ function Settings() {
     countPerRow,
     frameWidth,
   }) => {
-    document.documentElement.style.setProperty('--gw-wall-color', wallColor);
-    document.documentElement.style.setProperty('--gw-frame-color', frameColor);
-    document.documentElement.style.setProperty('--gw-count-per-row', countPerRow.toString());
-    document.documentElement.style.setProperty('--gw-frame-width', `${frameWidth.toString()}px`);
+    document.documentElement.style.setProperty('--gw-wall-color', localStorage.getItem('wallColor') || wallColor);
+    document.documentElement.style.setProperty('--gw-frame-color', localStorage.getItem('frameColor') || frameColor);
+    document.documentElement.style.setProperty('--gw-count-per-row', localStorage.getItem('countPerRow') || countPerRow);
+    document.documentElement.style.setProperty('--gw-frame-width', `${localStorage.getItem('frameWidth') || frameWidth.toString()}px`);
   }, []);
 
   
@@ -149,7 +167,10 @@ function Settings() {
           <input
             type="color"
             value={wallColor}
-            onChange={(e) => setWallColor(e.target.value)}
+            onChange={(e) => {
+              setWallColor(e.target.value)
+              localStorage.setItem('wallColor', e.target.value)
+            }}
           />
         </div>
 
@@ -158,7 +179,10 @@ function Settings() {
           <input
             type="color"
             value={frameColor}
-            onChange={(e) => setFrameColor(e.target.value)}
+            onChange={(e) => {
+              setFrameColor(e.target.value)
+              localStorage.setItem('frameColor', e.target.value)
+            }}
           />
         </div>
 
@@ -168,7 +192,10 @@ function Settings() {
             type="number"
             className="text-right"
             value={frameWidth}
-            onChange={(e) => setFrameWidth(Number(e.target.value))}
+            onChange={(e) => {
+              setFrameWidth(Number(e.target.value))
+              localStorage.setItem('frameWidth', e.target.value)
+            }}
           />
         </div>
 
@@ -178,7 +205,10 @@ function Settings() {
             type="number"
             className="text-right"
             value={countPerRow}
-            onChange={(e) => setCountPerRow(Number(e.target.value))}
+            onChange={(e) => {
+              setCountPerRow(Number(e.target.value))
+              localStorage.setItem('countPerRow', e.target.value)
+            }}
           />
         </div>
       </div>
